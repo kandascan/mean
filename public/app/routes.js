6f2bd665fa0d0a9d8947ad3237678950bcd38a00
kandascan <boguslaw.boczkowski@gmail.com>
2017-04-12 12:51:54 +0200

    implemented restricted routes for login, register and profile page

--- public/app/routes.js
+++ public/app/routes.js
@@ -1,43 +1,66 @@
-angular.module('appRoutes', ['ngRoute'])
+var app = angular.module('appRoutes', ['ngRoute'])
 
-.config(function($routeProvider, $locationProvider){
+    .config(function ($routeProvider, $locationProvider) {
 
-    $routeProvider
+        $routeProvider
 
-    .when('/', {
-        templateUrl: 'app/views/pages/home.html'
-    })
+            .when('/', {
+                templateUrl: 'app/views/pages/home.html'
+            })
 
-    .when('/about', {
-        templateUrl: 'app/views/pages/about.html'
-    })
+            .when('/about', {
+                templateUrl: 'app/views/pages/about.html'
+            })
 
-    .when('/addCost', {
-        templateUrl: 'app/views/pages/costs/addCost.html'
-    })
+            .when('/addCost', {
+                templateUrl: 'app/views/pages/costs/addCost.html'
+            })
 
-    .when('/register', {
-        templateUrl: 'app/views/pages/users/register.html',
-        controller: 'registrationController',
-        controllerAs: 'register'
-    })
+            .when('/register', {
+                templateUrl: 'app/views/pages/users/register.html',
+                controller: 'registrationController',
+                controllerAs: 'register',
+                authentidacted: false
+            })
 
-    .when('/login', {
-        templateUrl: 'app/views/pages/users/login.html'
-    })
+            .when('/login', {
+                templateUrl: 'app/views/pages/users/login.html',
+                authentidacted: false
 
-    .when('/logout', {
-        templateUrl: 'app/views/pages/users/logout.html'
-    })
+            })
 
-    .when('/profile', {
-        templateUrl: 'app/views/pages/users/profile.html'
-    })
-    
-    .otherwise({ redirectTo: '/'});
+            .when('/logout', {
+                templateUrl: 'app/views/pages/users/logout.html',
+                authentidacted: true
 
-    $locationProvider.html5Mode({ //dont show # in url 
-        enabled: true,
-        requireBase: false
+            })
+
+            .when('/profile', {
+                templateUrl: 'app/views/pages/users/profile.html',
+                authentidacted: true
+
+            })
+
+            .otherwise({ redirectTo: '/' });
+
+        $locationProvider.html5Mode({ //dont show # in url 
+            enabled: true,
+            requireBase: false
+        });
     });
-});
+
+app.run(['$rootScope', 'Auth', '$location', function ($rootScope, Auth, $location) {
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        if (next.$$route.authentidacted === true) {
+            if (!Auth.isLoggedIn()) {
+                event.preventDefault();
+                $location.path('/login');
+            }
+        } else if (next.$$route.authentidacted === false) {
+            if (Auth.isLoggedIn()) {
+                event.preventDefault();
+                $location.path('/');
+            }
+        }
+    });
+}]);
