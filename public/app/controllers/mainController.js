e44f2874cc45c5fe3b78fb3ef092ab71178d901e
kandascan <boguslaw.boczkowski@gmail.com>
2017-04-30 20:55:11 +0200

    fixed bug with not deploying properly to herokuapp

--- public/app/controllers/mainController.js
+++ public/app/controllers/mainController.js
@@ -1,77 +1,79 @@
-angular.module('mainController',['authServices'])
+angular.module('mainController', ['authServices'])
 
-.controller('mainCtrl', function(Auth, $timeout, $location, $rootScope, $scope, $http) {
-    var app = this;
-    app.loadme = false;
+    .controller('mainCtrl', function (Auth, $timeout, $location, $rootScope, $scope, $http) {
+        var app = this;
+        app.loadme = false;
 
-    $rootScope.currentuser = '';
+        $rootScope.currentuser = '';
 
-    $scope.cost = {username: '', costname: '', costprice: ''};
-    $scope.addCost = function(){
-      $scope.cost.username = $rootScope.currentuser;
+        $scope.cost = { username: '', costname: '', costprice: '' };
+        $scope.addCost = function () {
+            $scope.cost.username = $rootScope.currentuser;
 
-      $scope.loading = true;
-      $scope.errorMsg = false;
-      $scope.successMsg = false;
-      $http.post('/api/costs', $scope.cost).then(function(data){
-        $scope.cost = {username: '', costname: '', costprice: ''};
-        if(data.data.success) {
-            $scope.successMsg = data.data.message;
-            $scope.loading = false;
-            $timeout(function() {
-                $location.path('/');
-            }, 1000);
-        } else {
-            $scope.errorMsg = data.data.message;
-            $scope.loading = false;
-        }
-      });
+            $scope.loading = true;
+            $scope.errorMsg = false;
+            $scope.successMsg = false;
+            $http.post('/api/costs', $scope.cost).then(function (data) {
+                $scope.cost = { username: '', costname: '', costprice: '' };
+                if (data.data.success) {
+                    $scope.successMsg = data.data.message;
+                    $scope.loading = false;
+                    $timeout(function () {
+                        $location.path('/');
+                        $scope.successMsg = false;
 
-    };
-
-    $rootScope.$on('$routeChangeStart', function() {
-        if(Auth.isLoggedIn()) {
-            app.isLoggedIn = true;
-            Auth.getUser().then(function(data) {
-              $rootScope.currentuser = data.data.username;
-                app.username = data.data.username;
-                app.useremail = data.data.email;
-                app.loadme = true;
+                    }, 1000);
+                } else {
+                    $scope.errorMsg = data.data.message;
+                    $scope.loading = false;
+                }
             });
-        } else {
-            app.isLoggedIn = false;
-            app.username = '';
-            app.loadme = true;
-        }
-    });
 
-    this.doLogin = function(loginData){
-        app.loading = true;
-        app.errorMsg = false;
-        app.successMsg = false;
+        };
 
-        Auth.login(app.loginData).then(function(data){
-
-            if(data.data.success) {
-                app.successMsg = data.data.message;
-                app.loading = false;
-                $timeout(function() {
-                    $location.path('/profile');
-                    app.loginData = {};
-                    app.successMsg = false;
-                }, 1000);
+        $rootScope.$on('$routeChangeStart', function () {
+            if (Auth.isLoggedIn()) {
+                app.isLoggedIn = true;
+                Auth.getUser().then(function (data) {
+                    $rootScope.currentuser = data.data.username;
+                    app.username = data.data.username;
+                    app.useremail = data.data.email;
+                    app.loadme = true;
+                });
             } else {
-                app.errorMsg = data.data.message;
-                app.loading = false;
+                app.isLoggedIn = false;
+                app.username = '';
+                app.loadme = true;
             }
         });
-    };
 
-    this.logout = function() {
-        Auth.logout();
-        $location.path('/logout');
-        $timeout(function(){
-            $location.path('/');
-        }, 2000);
-    };
-});
+        this.doLogin = function (loginData) {
+            app.loading = true;
+            app.errorMsg = false;
+            app.successMsg = false;
+
+            Auth.login(app.loginData).then(function (data) {
+
+                if (data.data.success) {
+                    app.successMsg = data.data.message;
+                    app.loading = false;
+                    $timeout(function () {
+                        $location.path('/profile');
+                        app.loginData = {};
+                        app.successMsg = false;
+                    }, 1000);
+                } else {
+                    app.errorMsg = data.data.message;
+                    app.loading = false;
+                }
+            });
+        };
+
+        this.logout = function () {
+            Auth.logout();
+            $location.path('/logout');
+            $timeout(function () {
+                $location.path('/');
+            }, 2000);
+        };
+    });
